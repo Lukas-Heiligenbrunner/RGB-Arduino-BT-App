cc872e61bd614f121af07be86acda1bc87d99add
Lukas-Heiligenbrunner <ultimategameingcookie@gmail.com>
2017-09-20 20:09:15 +0200

    added menu with some buttons.

--- app/src/main/java/com/example/lukas/tabbedapp/MainActivity.java
+++ app/src/main/java/com/example/lukas/tabbedapp/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -60,6 +62,33 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_info) {
+
+        }
+        if (id == R.id.menu_howto) {
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
     void initialize_screen_orientation()
     {
